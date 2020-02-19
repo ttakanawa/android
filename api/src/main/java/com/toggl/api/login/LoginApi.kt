ea178635ca673f38a7b8dcda5d63badd7a5baf04
William Barbosa <heytherewill@gmail.com>
2020-02-19 08:35:20 -0300

    :earth_americas: Add environment related modules

---
+++ api/src/main/java/com/toggl/api/login/LoginApi.kt
@@ -0,0 +1,20 @@
+package com.toggl.api.login
+
+import com.toggl.models.domain.User
+import com.toggl.models.validation.ApiToken
+import com.toggl.models.validation.Email
+import com.toggl.models.validation.Password
+import com.toggl.models.validation.toApiToken
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import java.util.concurrent.TimeUnit
+
+class MockLoginApi : ILoginApi {
+    override fun login(email: Email.Valid, password: Password.Valid): Single<User> =
+        Single.timer(4, TimeUnit.SECONDS).flatMap {
+            when (val token = "12345678901234567890123456789012".toApiToken()) {
+                is ApiToken.Valid -> Single.just(User(token))
+                ApiToken.Invalid -> Single.error(NotImplementedError())
+            }
+        }
+}
