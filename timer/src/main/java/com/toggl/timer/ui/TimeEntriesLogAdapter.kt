b2ec45a0c26c5cb62b0358bbb87db657aad79b78
William Barbosa <heytherewill@gmail.com>
2020-02-27 18:03:16 -0300

    :art: Small UI cleanup

---
+++ timer/src/main/java/com/toggl/timer/ui/TimeEntriesLogAdapter.kt
@@ -0,0 +1,76 @@
+package com.toggl.timer.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.toggl.timer.R
+import com.toggl.timer.domain.states.FlatTimeEntryItem
+import com.toggl.timer.domain.states.TimeEntryLogViewModel
+import java.util.concurrent.TimeUnit
+
+
+class TimeEntryLogAdapter(val onContinueTappedListener: (Long) -> Unit = {})
+    : ListAdapter<TimeEntryLogViewModel, TimeEntryLogAdapter.TimeEntryLogViewHolder>(diffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeEntryLogViewHolder =
+        LayoutInflater.from(parent.context)
+            .inflate(R.layout.time_entries_log_item, parent, false)
+            .let(::TimeEntryLogViewHolder)
+
+    override fun onBindViewHolder(holder: TimeEntryLogViewHolder, position: Int) {
+        holder.bind(getItem(position) as FlatTimeEntryItem)
+    }
+
+    inner class TimeEntryLogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val addDescriptionLabel = itemView.findViewById<View>(R.id.addDescriptionLabel)
+        private val description = itemView.findViewById<TextView>(R.id.description)
+        private val duration = itemView.findViewById<TextView>(R.id.duration)
+        private val continueButton = itemView.findViewById<View>(R.id.continueButton)
+
+        fun bind(item: FlatTimeEntryItem) {
+
+            val hasDescription = item.description.isNotBlank()
+            addDescriptionLabel.isVisible = !hasDescription
+            description.isVisible = hasDescription
+            description.text = item.description
+
+            item.duration?.let { durationMillis ->
+                val hms = String.format(
+                    "%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(durationMillis),
+                    TimeUnit.MILLISECONDS.toMinutes(durationMillis) % TimeUnit.HOURS.toMinutes(1),
+                    TimeUnit.MILLISECONDS.toSeconds(durationMillis) % TimeUnit.MINUTES.toSeconds(1)
+                )
+                duration.text = hms
+            }
+
+            continueButton.setOnClickListener {
+                onContinueTappedListener(item.id)
+            }
+        }
+    }
+
+    companion object {
+        val diffCallback =  object : DiffUtil.ItemCallback<TimeEntryLogViewModel>() {
+            override fun areItemsTheSame(
+                oldItem: TimeEntryLogViewModel,
+                newItem: TimeEntryLogViewModel
+            ): Boolean =
+                oldItem is FlatTimeEntryItem &&
+                        newItem is FlatTimeEntryItem &&
+                        oldItem.id  == newItem.id
+
+            override fun areContentsTheSame(
+                oldItem: TimeEntryLogViewModel,
+                newItem: TimeEntryLogViewModel
+            ): Boolean =
+                oldItem is FlatTimeEntryItem &&
+                newItem is FlatTimeEntryItem &&
+                oldItem.description == newItem.description
+        }
+
+    }
+}
