0ab75ad116c6794c79c87749c979ab17eac0fad4
William Barbosa <heytherewill@gmail.com>
2020-03-06 16:20:53 -0300

    :lipstick: Split files

--- timer/src/main/java/com/toggl/timer/log/ui/TimeEntriesLogAdapter.kt
+++ timer/src/main/java/com/toggl/timer/log/ui/TimeEntriesLogAdapter.kt
@@ -1,67 +1,22 @@
 package com.toggl.timer.log.ui
 
-import android.content.res.ColorStateList
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.toColorInt
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.toggl.timer.R
 import com.toggl.timer.log.domain.FlatTimeEntryItem
 import com.toggl.timer.log.domain.TimeEntryViewModel
-import java.util.concurrent.TimeUnit
 
-class TimeEntryLogAdapter(val onContinueTappedListener: (Long) -> Unit = {})
-    : ListAdapter<TimeEntryViewModel, TimeEntryLogAdapter.TimeEntryLogViewHolder>(diffCallback) {
+class TimeEntryLogAdapter(private val onContinueTappedListener: (Long) -> Unit = {})
+    : ListAdapter<TimeEntryViewModel, TimeEntryLogViewHolder>(diffCallback) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeEntryLogViewHolder =
         LayoutInflater.from(parent.context)
             .inflate(R.layout.time_entries_log_item, parent, false)
-            .let(::TimeEntryLogViewHolder)
+            .let { TimeEntryLogViewHolder(it, onContinueTappedListener) }
 
     override fun onBindViewHolder(holder: TimeEntryLogViewHolder, position: Int) {
         holder.bind(getItem(position) as FlatTimeEntryItem)
-    }
-
-    inner class TimeEntryLogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val addDescriptionLabel = itemView.findViewById<View>(R.id.add_description_label)
-        private val project = itemView.findViewById<TextView>(R.id.project_label)
-        private val description = itemView.findViewById<TextView>(R.id.description)
-        private val duration = itemView.findViewById<TextView>(R.id.duration)
-        private val continueButton = itemView.findViewById<View>(R.id.continue_button)
-
-        fun bind(item: FlatTimeEntryItem) {
-
-            val hasDescription = item.description.isNotBlank()
-            addDescriptionLabel.isVisible = !hasDescription
-            description.isVisible = hasDescription
-            description.text = item.description
-
-            if (item.project == null) {
-                project.isVisible = false
-            } else {
-                project.isVisible = true
-                project.text = item.project.name
-                project.setTextColor(item.project.color.toColorInt())
-            }
-
-            item.duration?.let { durationMillis ->
-                val hms = String.format(
-                    "%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(durationMillis),
-                    TimeUnit.MILLISECONDS.toMinutes(durationMillis) % TimeUnit.HOURS.toMinutes(1),
-                    TimeUnit.MILLISECONDS.toSeconds(durationMillis) % TimeUnit.MINUTES.toSeconds(1)
-                )
-                duration.text = hms
-            }
-
-            continueButton.setOnClickListener {
-                onContinueTappedListener(item.id)
-            }
-        }
     }
 
     companion object {
@@ -82,6 +37,5 @@ class TimeEntryLogAdapter(val onContinueTappedListener: (Long) -> Unit = {})
                 newItem is FlatTimeEntryItem &&
                 oldItem.description == newItem.description
         }
-
     }
 }
---
+++ timer/src/main/java/com/toggl/timer/log/ui/TimeEntriesLogViewHolder.kt
@@ -0,0 +1,50 @@
+package com.toggl.timer.log.ui
+
+import android.view.View
+import android.widget.TextView
+import androidx.core.graphics.toColorInt
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.toggl.timer.R
+import com.toggl.timer.log.domain.FlatTimeEntryItem
+import java.util.concurrent.TimeUnit
+
+class TimeEntryLogViewHolder(
+    itemView: View,
+    private val onContinueTappedListener: (Long) -> Unit
+) : RecyclerView.ViewHolder(itemView) {
+    private val addDescriptionLabel = itemView.findViewById<View>(R.id.add_description_label)
+    private val project = itemView.findViewById<TextView>(R.id.project_label)
+    private val description = itemView.findViewById<TextView>(R.id.description)
+    private val duration = itemView.findViewById<TextView>(R.id.duration)
+    private val continueButton = itemView.findViewById<View>(R.id.continue_button)
+
+    fun bind(item: FlatTimeEntryItem) {
+
+        val hasDescription = item.description.isNotBlank()
+        addDescriptionLabel.isVisible = !hasDescription
+        description.isVisible = hasDescription
+        description.text = item.description
+
+        if (item.project == null) {
+            project.isVisible = false
+        } else {
+            project.isVisible = true
+            project.text = item.project.name
+            project.setTextColor(item.project.color.toColorInt())
+        }
+
+        item.duration?.let { durationMillis ->
+            val hms = String.format(
+                "%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(durationMillis),
+                TimeUnit.MILLISECONDS.toMinutes(durationMillis) % TimeUnit.HOURS.toMinutes(1),
+                TimeUnit.MILLISECONDS.toSeconds(durationMillis) % TimeUnit.MINUTES.toSeconds(1)
+            )
+            duration.text = hms
+        }
+
+        continueButton.setOnClickListener {
+            onContinueTappedListener(item.id)
+        }
+    }
+}
