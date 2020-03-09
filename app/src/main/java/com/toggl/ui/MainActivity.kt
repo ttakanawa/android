7a8c786853e94495be71eb76af374c519c196c66
Tomáš Valenta <semanticer@gmail.com>
2020-03-09 20:17:56 +0800

    :construction: Make our UI hierarchy flatter (#18)

--- app/src/main/java/com/toggl/ui/MainActivity.kt
+++ app/src/main/java/com/toggl/ui/MainActivity.kt
@@ -1,25 +1,46 @@
 package com.toggl.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.toggl.R
 import com.toggl.TogglApplication
 import com.toggl.domain.AppAction
 import com.toggl.domain.AppState
 import com.toggl.architecture.core.Store
+import com.toggl.common.DeepLinkUrls
+import kotlinx.android.synthetic.main.main_activity.*
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
     @Inject
     lateinit var store: Store<AppState, AppAction>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
 
         (applicationContext as TogglApplication).appComponent.inject(this)
 
         store.dispatch(AppAction.Load)
+
+        bottom_navigation.setOnNavigationItemSelectedListener(::changeTab)
+        bottom_navigation.setOnNavigationItemReselectedListener(::scrollUpOnTab)
+    }
+
+    private fun changeTab(menuItem: MenuItem): Boolean {
+        nav_host_fragment.findNavController().navigate(
+            when (menuItem.itemId) {
+                R.id.timer_tab -> DeepLinkUrls.timeEntriesLog
+                R.id.reports_tab -> DeepLinkUrls.reports
+                R.id.calendar_tab -> DeepLinkUrls.calendar
+                else -> throw NotImplementedError()
+            }
+        )
+        return true
+    }
+
+    private fun scrollUpOnTab(menuItem: MenuItem) {
     }
 }
